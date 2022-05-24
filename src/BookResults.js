96527ea59521f024a3c33299ec24d57c8f54e494
DYLAN ULLRICH <dylan@getullrichordietrying.com>
2022-05-24 13:24:31 -0700

    removed ; and changed text color

--- src/BookResults.js
+++ src/BookResults.js
@@ -1,39 +1,27 @@
 import React from 'react';
-import {Carousel} from 'react-bootstrap';
+import { Carousel } from 'react-bootstrap';
+import './BookResults.css';
 
 class BookResults extends React.Component {
-
-  constructor(props){
-    super(props)
-    this.state={
-      bookData:[]
-    }
-  }
-
-  static getDerivedStateFromProps(props, state) {
-    return {
-        bookData: props.bookData
-    }
-}
-
-
   render() {
-    const{bookData} = this.state
-    console.log(bookData)
+    const { bookData } = this.props;
+    console.log(bookData);
     return (
       <>
         <Carousel>
           {bookData.map((displayedBook, idx) => (
-            <Carousel.Item interval={1500} key={idx} >
+            <Carousel.Item interval={1500} key={idx}>
               <Carousel.Caption>
                 <div>{displayedBook.title}</div>
                 <div>{displayedBook.description}</div>
                 <div>{displayedBook.status}</div>
               </Carousel.Caption>
-                <img src='https://media.istockphoto.com/photos/composition-with-books-on-the-table-picture-id1158413597?k=20&m=1158413597&s=612x612&w=0&h=NYk7P4GACfJ51tXQd3kLUrFski-z-Ykn1tZRWSlCcow=' alt={this.props.bookData.title}></img>
+              <img
+                src="https://media.istockphoto.com/photos/composition-with-books-on-the-table-picture-id1158413597?k=20&m=1158413597&s=612x612&w=0&h=NYk7P4GACfJ51tXQd3kLUrFski-z-Ykn1tZRWSlCcow="
+                alt={this.props.bookData.title}
+              ></img>
             </Carousel.Item>
           ))}
-          ;
         </Carousel>
         <div>{this.props.bookData.title}</div>
       </>
